9240b5a449160079ba37bb3080101e65c8424af2
parteek kumar <digitalparteek@gmail.com>
2023-07-05 11:29:06 +0530

    Added problems

---
+++ PatternMatching/addStringPalin.java
@@ -0,0 +1,11 @@
+package PatternMatching;
+
+// question
+
+// given a string of len N , calculate the minimum number of chars needed to add at
+// start to make entire String as Palindrome
+public class addStringPalin {
+    public static void main(String[] args) {
+
+    }
+}
--- PatternMatching/lpsBrute.java
+++ PatternMatching/lpsBrute.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 
 public class lpsBrute {
 
-    static int[] computeLPS(String pattern) {
+    static int[] computeLPS(String text) {
         int len = 0;
         int i = 1;
-        int[] lps = new int[pattern.length()];
+        int[] lps = new int[text.length()];
 
-        while (i < pattern.length()) {
-            if (pattern.charAt(i) == pattern.charAt(len)) {
+        while (i < text.length()) {
+            if (text.charAt(i) == text.charAt(len)) {
                 len++;
                 lps[i] = len;
                 i++;
@@ -29,20 +29,26 @@ public class lpsBrute {
     }
 
     static int bruteForceSubstringCount(String text, String pattern) {
-        int[] lps = computeLPS(pattern);
+        int[] lps = computeLPS(text);
         int n = text.length();
         int m = pattern.length();
         int count = 0;
+        int i =0, j=0;
 
-        for (int i = 0; i <= n - m; i++) {
-            int j;
-            for (j = 0; j < m; j++) {
-                if (text.charAt(i + j) != pattern.charAt(j)) {
-                    break;
-                }
+        while (i<n){
+            if(text.charAt(i) == pattern.charAt(j)){
+                i++;
+                j++;
             }
-            if (j == m) {
+            if(j==m){
                 count++;
+                j= lps[j-1];
+            } else if (i<n && text.charAt(i) != pattern.charAt(j)) {
+                if(j != 0){
+                    j = lps[j-1];
+                }else {
+                    i++;
+                }
             }
         }
 
@@ -57,8 +63,8 @@ public class lpsBrute {
 //        }else {
 //            System.out.println("Pattern not found in the text.");
 //        }
-        String text = "abacaabacdacd";
-        String pattern = "acd";
+        String text = "asdsaaccacacacaadc";
+        String pattern = "ac";
 
         int count = bruteForceSubstringCount(text, pattern);
         System.out.println("Substring count: " + count);
