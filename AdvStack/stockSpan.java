32e2fdaddbe4dc42acc8e2797ee54212d8045b00
parteek kumar <digitalparteek@gmail.com>
2023-07-12 02:45:17 +0530

    Added problems

--- AdvStack/stockSpan.java
+++ AdvStack/stockSpan.java
@@ -1,6 +1,7 @@
 package AdvStack;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class stockSpan {
 
@@ -18,9 +19,32 @@ public class stockSpan {
         }
         return span;
     }
+
+    static int[] stackSpan(int[] arr){
+        int n = arr.length;
+        int[] span = new int[n];
+        Stack<Integer> sc = new Stack<>();
+        for(int i=0; i<n; i++){
+            while (!sc.empty() && arr[i] >= arr[sc.peek()]){
+                sc.pop();
+            }
+            if(!sc.empty()){
+                span[i] = i-sc.peek(); // subtract the current index from stack index stored;
+            }else{
+                span[i] = i+1;
+            }
+            sc.push(i);
+
+
+        }
+        return span;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {100,80,60,70,60,75,85};
-        int[] res = bruteSpan(arr);
+//        int[] res = bruteSpan(arr);
+        int[] res = stackSpan(arr);
         System.out.println(Arrays.toString(res));
     }
 }
