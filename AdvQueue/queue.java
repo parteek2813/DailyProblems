bbc34cc2220778a2cb7704c1791f80cac19d52a2
parteek kumar <digitalparteek@gmail.com>
2023-07-13 23:57:11 +0530

    added problems

---
+++ AdvQueue/queue.java
@@ -0,0 +1,62 @@
+package AdvQueue;
+
+class QNode {
+    int data;
+    QNode next;
+
+
+    public QNode(int d) {
+        this.data = d;
+        this.next = null;
+    }
+
+}
+
+class Queue {
+    QNode head, tail;
+
+    public Queue() {
+        this.head = this.tail = null;
+        int size =0;
+
+    }
+
+    void enqueue(int data){
+
+        QNode temp = new QNode(data);
+
+        // if queue is empty,then new node will point to head and tail both
+
+        if(this.head == null){
+            this.head = this.tail = temp;
+
+        }else{
+            tail.next = new QNode(data);
+             tail = tail.next;
+        }
+
+    }
+
+    void dequeue(){
+
+        if(this.head == null){
+            return;
+        }
+
+        QNode temp = this.head;
+        this.head = this.head.next;
+
+        // make both head and tail null
+        if(this.head == null){
+            this.tail = null;
+        }
+
+
+        // IN C++ , No garbage collection
+        // So to clear up ---> make new temp and then make temp.next = null!
+    }
+}
+
+public class queue {
+
+}
