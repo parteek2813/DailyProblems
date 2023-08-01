14f40b8c653f443a99eb1426edf4d825f7ea19ea
parteek kumar <digitalparteek@gmail.com>
2023-08-01 20:01:04 +0530

    added problems

--- AdvDp/MacChopstick.java
+++ AdvDp/MacChopstick.java
@@ -32,11 +32,34 @@ public class MacChopstick {
         return count;
     }
 
+    static int MaxchopRec(int[] chopStick, int d, int idx) {
+        int n = chopStick.length;
+
+        // Base case: If all chopsticks are considered or there's only one chopstick left
+        if (idx >= n - 1) {
+            return 0;
+        }
+
+        // If the current chopstick can be paired with the next one
+        int countIfPaired = 0;
+        if (Math.abs(chopStick[idx] - chopStick[idx + 1]) <= d) {
+            countIfPaired = 1 + MaxchopRec(chopStick, d, idx + 2);
+        }
+
+        // If the current chopstick is not paired with the next one
+        int countIfNotPaired = MaxchopRec(chopStick, d, idx + 1);
+
+        // Return the maximum count among the two possibilities
+        return Math.max(countIfPaired, countIfNotPaired);
+    }
+
+
 
     public static void main(String[] args) {
         int[] chopStick = {5, 3, 10, 15, 12, 14, 18};
         int d = 3;
         int res = Maxchop(chopStick, d);
+//        int res = MaxchopRec(chopStick, d, 0);
 
 
         System.out.println(res);
