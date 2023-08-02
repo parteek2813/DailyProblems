daf08ea1ae2d98e8ceec0a0462f18c0d8c46be5d
parteek kumar <digitalparteek@gmail.com>
2023-08-02 15:35:18 +0530

    added problems

--- AdvBacktracking/countSubsetsPrime.java
+++ AdvBacktracking/countSubsetsPrime.java
@@ -6,6 +6,9 @@ package AdvBacktracking;
 // Given a array of size n .... we have to construct the subset such that the sum of the element
 // of the subset is a Prime number. Calculate the count of number of prime number of sum of elemenets
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class countSubsetsPrime {
 
     public static boolean isPrime(int num){
@@ -52,11 +55,45 @@ public class countSubsetsPrime {
         generateSubsets(arr, 0, 0, count);
         return count[0];
     }
+
+
+    public static void printAllSubsets(int[] arr, int index, List<Integer> currentSubset,List<List<Integer>> subsets){
+        subsets.add(new ArrayList<>(currentSubset));
+
+        // logic
+        for(int i =index; i<arr.length; i++){
+            currentSubset.add(arr[i]);
+
+            // rec call
+            printAllSubsets(arr, i+1, currentSubset, subsets);
+
+            // for exact backtracking
+            currentSubset.remove(currentSubset.size() -1);
+
+        }
+
+
+    }
+    public static List<List<Integer>> getSubsets(int[] arr){
+        // define the arraylist here
+        List<List<Integer>> subsets = new ArrayList<>();
+        printAllSubsets(arr, 0, new ArrayList<>(), subsets);
+        return subsets;
+   }
+
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 4};
-        int res = countSubsetsPrimeActual(arr);
-        System.out.println(res);
+//        int res = countSubsetsPrimeActual(arr);
+//        System.out.println(res);
+        List<List<Integer>> subsets = getSubsets(arr);
+        for(List<Integer> subset: subsets){
+            System.out.println(subset);
+        }
+
+
     }
+
+
 
     // subsets of {1,3,2,4}
     // []
