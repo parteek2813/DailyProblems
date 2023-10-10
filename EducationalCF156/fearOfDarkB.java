599bc13cc6a765a4a40121c24d3ba753fae1852c
parteek kumar <digitalparteek@gmail.com>
2023-10-10 11:14:13 +0530

    Added problems

---
+++ EducationalCF156/fearOfDarkB.java
@@ -0,0 +1,72 @@
+package EducationalCF156;
+
+import java.text.DecimalFormat;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class fearOfDarkB {
+
+    public static void solve(){
+        Scanner sc = new Scanner(System.in);
+        int px = sc.nextInt();
+        int py = sc.nextInt();
+
+        int ax = sc.nextInt();
+        int ay = sc.nextInt();
+        int bx = sc.nextInt();
+        int by = sc.nextInt();
+
+        // Case when we consider a sinlge circle covers both the point P and O by
+        // only one of the lanterns A or B
+
+        int ap=(ax-px)*(ax-px) + (ay-py)*(ay-py);
+        int bp=(bx-px)*(bx-px) + (by-py)*(by-py);
+        int ao=(ax)*(ax) + (ay)*(ay);
+        int bo=(bx)*(bx) + (by)*(by);
+
+
+//      // storing ans from considering one circle as the Minimum of both max
+        int ans = Math.min(Math.max(ap,ao), Math.max(bp,ao));
+
+        int ab=(ax-bx)*(ax-bx) + (ay-by)*(ay-by);
+
+        double an1 = ans;
+        double an3 = ans;
+
+        int val1 = Math.max(bp, ao);
+        double dis = (Math.sqrt(ab))/2.0;
+        double dis1 = Math.sqrt(val1);
+        an1 = Math.max(dis,dis1);
+
+        int val2 = Math.max(bo, ap);
+        double dis3 = Math.sqrt(val2);
+        an3 = Math.max(dis,dis3);
+
+
+        double an2 = Math.sqrt(ans);
+
+
+        an2 = Math.min(an2, an1);
+        an2 = Math.min(an2, an3);
+
+//        DecimalFormat df = new DecimalFormat("#.############"); // 12 decimal places
+//        String formattedNumber = df.format(an2);
+//        System.out.println(formattedNumber);
+
+
+        Formatter fm = new Formatter();
+
+        fm.format("%.10f", an2);
+        System.out.println(fm);
+        fm.close();
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0){
+            solve();
+        }
+    }
+}
