8612971455fd45389e093fcfd78efea1d9f46ab0
parteek kumar <digitalparteek@gmail.com>
2023-10-17 16:45:19 +0530

    Added problems

--- dpCollection/frogJump3.java
+++ dpCollection/frogJump3.java
@@ -1,5 +1,7 @@
 package dpCollection;
 
+import java.util.Arrays;
+
 /*
 Technique to solve in 3 steps
 
@@ -7,6 +9,10 @@ Technique to solve in 3 steps
 2. Do all stuffs on that index
 3. Take the minimum(on all stuffs)
 
+
+Recursive to DP soln :
+
+- Look at the parameters changing
  */
 public class frogJump3 {
 
@@ -27,13 +33,43 @@ public class frogJump3 {
         return Math.min(left, right);
 
     }
+
+
+    public static int frogJumpMemo(int[] arr, int n , int[] dp){
+
+        // TIME- O(N)
+        // SPACE- O(N) + O(N)
+
+        // base case
+        if(n==0) return 0;
+
+        // dp case
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        // rec case
+        int left = frogJumpMemo(arr, n-1, dp) +  Math.abs(arr[n] - arr[n-1]);
+
+        if(n>1){
+            right = frogJumpMemo(arr, n-2,dp) + Math.abs(arr[n] - arr[n-2]);
+        }
+
+        return dp[n] = Math.min(left, right);
+
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {10,20,30,10};
         int n = arr.length;
-
-
         int result = frogJumpRec(arr, n-1);
-        System.out.println(result);
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        int res = frogJumpMemo(arr, n-1, dp);
+
+        System.out.println(res);
     }
 }
