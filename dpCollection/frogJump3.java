619c77cc428892ff396df6fde79c2fb4435485b8
parteek kumar <digitalparteek@gmail.com>
2023-10-17 18:44:01 +0530

    Added problems

--- dpCollection/frogJump3.java
+++ dpCollection/frogJump3.java
@@ -13,6 +13,13 @@ Technique to solve in 3 steps
 Recursive to DP soln :
 
 - Look at the parameters changing
+
+
+ SPACE OPTIMIZATION THUMB RULE:
+
+ Anytime there is something index-1 / index-2 , there would always be space optimization
+ in which we can use only 2-3 variable and reduce the space of array size
+
  */
 public class frogJump3 {
 
@@ -59,16 +66,37 @@ public class frogJump3 {
 
     }
 
+    public static int frogJumpMemoTab(int n , int[] arr){
+        int[] dp = new int[n+1];
+
+        Arrays.fill(dp,0);
+        dp[0] = 0;
+
+        for(int i=1; i<n; i++){
+            int fs = dp[i-1] + Math.abs(arr[i] - arr[i-1]);
+            int ss = Integer.MAX_VALUE;
+            if(i>1) ss = dp[i-2] + Math.abs(arr[i] - arr[i-2]);
+            dp[i] = Math.min(fs, ss);
+        }
+
+        return dp[n-1];
+
+    }
+
+
     public static void main(String[] args) {
 
         int[] arr = {10,20,30,10};
         int n = arr.length;
-        int result = frogJumpRec(arr, n-1);
+//        int result = frogJumpRec(arr, n-1);
 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+//        int[] dp = new int[n+1];
+//        Arrays.fill(dp, -1);
 
-        int res = frogJumpMemo(arr, n-1, dp);
+//        int res = frogJumpMemo(arr, n-1, dp);
+
+
+        int res = frogJumpMemoTab(n, arr);
 
         System.out.println(res);
     }
